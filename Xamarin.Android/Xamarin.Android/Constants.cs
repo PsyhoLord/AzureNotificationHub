f6aa7405f9f7aae2f7330b8532655c2ddaede865
Alex <psyholord@gmail.com>
2019-03-28 01:30:12 +0200

    Created project, added all classes from manual

---
+++ Xamarin.Android/Xamarin.Android/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Xamarin.Android
+{
+    public static class Constants
+    {
+        public const string ListenConnectionString = "Endpoint=sb://azurenotificationhubhsample.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=D5+vXDUiUIEgr5w0PKKG37LmeJ9Hb8FhCyyoM9acG5Q=";
+        public const string NotificationHubName = "<hub name>";
+    }
+}
